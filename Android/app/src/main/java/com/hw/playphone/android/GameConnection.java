3d049c72eb12b1585393fbd8198dd742d4a3e1a2
Ethan Madison <ethan@ethanmad.com>
2014-10-31 11:07:49 -0700

    added sendDiscovery() method

--- Android/app/src/main/java/com/hw/playphone/android/GameConnection.java
+++ Android/app/src/main/java/com/hw/playphone/android/GameConnection.java
@@ -1,5 +1,7 @@
 package com.hw.playphone.android;
 
+import android.util.Log;
+
 import com.hw.playphone.android.model.Request;
 import com.hw.playphone.android.model.UserData;
 
@@ -10,12 +12,13 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 /**
- * Created by ethan on 10/29/14.
- */
+* Created by ethan on 10/29/14.
+*/
 public class GameConnection {
     Socket socket;
     InputStream inputStream;
     OutputStream outputStream;
+    private Thread thread;
 
 
     public GameConnection(Socket socket) {
@@ -36,6 +39,7 @@ public class GameConnection {
             id.put("firstname", UserData.getValue("firstname"));
             id.put("lastname", UserData.getValue("lastname"));
             id.put("username", UserData.getValue("username"));
+            id.put("fbuid", "");
             r.data.put("id", id);
 
             r.data.put("APIVersion", 1);
@@ -44,5 +48,45 @@ public class GameConnection {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private int strlen(byte[] bytes, int off){
+        for(int i = 0; i < bytes.length-off; i++){
+            if(bytes[i+off]==0)return i;
+        }
+        return bytes.length-off;
+    }
+
+    private void handleMsg(String msg){
+        Log.d("", "received msg: "+msg);
+    }
+
+    public void listen() {
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                byte[] buffer = new byte[1024];
+                String cur = "";
+                try {
+                    while (socket.isConnected()) {
+                        int amt = inputStream.read(buffer);
+
+                        int bytesProcessed = 0;
+                        do {
+                            int length = (int) strlen(buffer, bytesProcessed);
+                            cur += new String(buffer, bytesProcessed, length);
+                            if(bytesProcessed+length < amt) {
+                                handleMsg(cur);
+                                cur = "";
+                            }
+                            bytesProcessed += length + 1;
+                        } while (bytesProcessed < amt);
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
     }
 }
